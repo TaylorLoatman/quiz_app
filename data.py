b13a1936c7eb1700e305b797db7bb56663d8952f
Taylor Loatman <taylor.loatman@icloud.com>
2021-10-03 15:43:29 -0400

    Add check anser function

--- data.py
+++ data.py
@@ -10,7 +10,6 @@ response.raise_for_status()
 data = response.json()
 
 question_data = data['results']
-print(question_data)
 
 # question_data = [
 #     {
--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,16 +16,41 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.canvas.create_text(150, 125, text="The text here", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="The text here", fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         correct_img = PhotoImage(file="images/true.png")
-        self.correct_button = Button(image=correct_img)
+        self.correct_button = Button(image=correct_img, command=self.true_pressed)
         self.correct_button.grid(column=0, row=2)
 
         wrong_img = PhotoImage(file="images/false.png")
-        self.wrong_button = Button(image=wrong_img)
+        self.wrong_button = Button(image=wrong_img, command=self.false_pressed)
         self.wrong_button.grid(column=1, row=2)
 
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
